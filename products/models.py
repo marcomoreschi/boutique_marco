3129e04e18d74f2399d8cbe554a54e4612faf896
marcomoreschi <marcomoreschi91@hotmail.it>
2020-07-07 09:34:34 +0000

    customized admin

--- products/models.py
+++ products/models.py
@@ -4,6 +4,10 @@ from django.db import models
 
 
 class Category(models.Model):
+
+    class Meta:
+        verobose_name_pliral = "Categories"
+
     name = models.CharField(max_length=254)
     friendly_name = models.CharField(max_length=254, null=True, blank=True)
 
