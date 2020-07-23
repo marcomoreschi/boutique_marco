2f00204869693274c6a21033da660b6c1310a90c
marcomoreschi <marcomoreschi91@hotmail.it>
2020-07-23 08:40:44 +0000

    product admin, add product view

--- checkout/admin.py
+++ checkout/admin.py
@@ -12,7 +12,8 @@ class OrderAdmin(admin.ModelAdmin):
 
     readonly_fields = ('order_number', 'date',
                        'delivery_cost', 'order_total',
-                       'grand_total', 'original_bag', 'stripe_pid')
+                       'grand_total', 'original_bag',
+                       'stripe_pid')
 
     fields = ('order_number', 'user_profile', 'date', 'full_name',
               'email', 'phone_number', 'country',
@@ -26,6 +27,5 @@ class OrderAdmin(admin.ModelAdmin):
                     'grand_total',)
 
     ordering = ('-date',)
-
 
 admin.site.register(Order, OrderAdmin)
