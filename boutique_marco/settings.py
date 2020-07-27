10436f9d85c232feaee885b750049a9eb90ff59c
marcomoreschi <marcomoreschi91@hotmail.it>
2020-07-27 09:08:17 +0000

    removed secret key

--- boutique_marco/settings.py
+++ boutique_marco/settings.py
@@ -21,10 +21,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '5z7)y+a&u4#c2iw1!kcme2vbiosi_2wsq(mlwx!=s4$_g$(=$0'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['marco-boutique.herokuapp.com', 'localhost']
 
